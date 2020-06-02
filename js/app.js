0be7d7797620e6a76a4c4e0ffc6e03e344e30504
Elmer Lopez <meme_lopez10@hotmail.com>
2020-06-02 16:17:26 -0600

    done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const video = document.querySelector('video');
 const listOfSpans = document.querySelectorAll('.lyrics span');
+const lyrics = document.querySelector('.lyrics');
 
 function stringToSec (hrsMinutes) {
     let scnds = 0;
@@ -14,11 +15,26 @@ function stringToSec (hrsMinutes) {
 }
 
 function back2Normal(){
+    console.log('corre back2normal()');
     for (let i = 0; i < listOfSpans.length; i++) {
         // const p = listOfSpans[i].firstElementChild.className="NHghlght";
-        const p = listOfSpans[i].firstElementChild.style.backgroundColor = 'white';
+        // if (listOfSpans[i].firstElementChild.style.backgroundColor === 'lightskyblue'){
+            // console.log('yes');
+            // listOfSpans[i].firstElementChild.style.backgroundColor = 'white';
+        // }
+        const p = listOfSpans[i].firstElementChild;
+        p.className = "";
     }
 }
+
+lyrics.addEventListener('click', (event) => {
+    const spanFather = event.target.parentNode;
+    if (event.target.tagName == 'P') {
+        console.log('click en: ' + event.target.innerHTML + 'el startT: ' + stringToSec(spanFather.dataset.start));
+        back2Normal();
+        video.currentTime = stringToSec(spanFather.dataset.start);
+    }
+});
 
 video.addEventListener('timeupdate', (event) => {
     let elCorrecto = -1;
@@ -31,12 +47,14 @@ video.addEventListener('timeupdate', (event) => {
         
         // console.log(listOfSpans[i].dataset.start + ' y el tiempo del video: ' + videoT + ' - stringToSecs: ' + stringToSec(listOfSpans[i].dataset.start));
         
-        console.log('i: ' + i + ' videoT: ' + videoT + ' startT: ' + startT + ' endT: ' + endT + ' text: ' + p.innerHTML);
+        // console.log('i: ' + i + ' videoT: ' + videoT + ' startT: ' + startT + ' endT: ' + endT + ' text: ' + p.innerHTML);
         if (videoT > startT ){
-            listOfSpans[i].style.backgroundColor = 'lightskyblue';
+            // listOfSpans[i].style.backgroundColor = 'lightskyblue';
+            p.className = "active"
         }
         if (videoT > endT) {
-            listOfSpans[i].style.backgroundColor = 'white';
+            // listOfSpans[i].style.backgroundColor = 'white';
+            p.className = ""
         }
     }
     // if (elCorrecto >= 0 ){
