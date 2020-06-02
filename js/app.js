fe3ed3dc3af75f73a1cfc8185370721f6aad5191
Elmer Lopez <meme_lopez10@hotmail.com>
2020-06-01 21:06:22 -0600

    mobile done. Trying to get the (highlight of the words when said in the video) part to work. also changed everything to a sass project.

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const video = document.querySelector('video');
+const listOfSpans = document.querySelectorAll('.lyrics span');
+
+function stringToSec (hrsMinutes) {
+    let scnds = 0;
+    const hrMin = hrsMinutes.split(':');
+    const secMl = hrMin[2].split(',');
+    const str = '0.'+ secMl[1];
+    scnds = parseFloat(secMl[0]) + parseFloat(str);
+
+    return (scnds);
+    // return ('horas: ' + hrMin[0] + ' - minutos: ' + hrMin[1] + ' - segundos: ' + secMl[0] + ' - milis:' + secMl[1]  + ' - sec: ' + str +' '+scnds);
+
+}
+
+function back2Normal(){
+    for (let i = 0; i < listOfSpans.length; i++) {
+        // const p = listOfSpans[i].firstElementChild.className="NHghlght";
+        const p = listOfSpans[i].firstElementChild.style.backgroundColor = 'white';
+    }
+}
+
+video.addEventListener('timeupdate', (event) => {
+    let elCorrecto = -1;
+    back2Normal();
+    for (let i = 0; i < listOfSpans.length; i++) {
+        let videoT = video.currentTime;
+        let startT = stringToSec(listOfSpans[i].dataset.start);
+        let endT = stringToSec(listOfSpans[i].dataset.end);
+        const p = listOfSpans[i].firstElementChild;
+        
+        // console.log(listOfSpans[i].dataset.start + ' y el tiempo del video: ' + videoT + ' - stringToSecs: ' + stringToSec(listOfSpans[i].dataset.start));
+        
+        console.log('i: ' + elCorrecto + ' videoT: ' + videoT + ' startT: ' + startT + ' endT: ' + endT + ' text: ' + p.innerHTML);
+        if (videoT > startT ){
+            if (videoT < endT) {
+                // console.log('entro a segundo if -- videoT: '+ videoT + ' startT: ' + startT);
+                // p.style.backgroundColor = 'lightskyblue';
+                elCorrecto = i;
+            } else {
+                elCorrecto = -1;
+            }
+        }
+    }
+    if (elCorrecto >= 0 ){
+        listOfSpans[elCorrecto].firstElementChild.style.backgroundColor = 'lightskyblue';
+        console.log('yes ' + elCorrecto);
+    } else {
+        console.log('no ' + elCorrecto);
+    }
+});
